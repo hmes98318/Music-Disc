313e1b38d9a4e3403e12d80e88baf761d0c1598b
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-02-04 06:10:33 +0800

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,27 @@
+const embed = require('../embeds/embeds.js');
+
 module.exports = (client, int) => {
-    if (!int.isButton()) return;
+    if (!int.isButton())
+        return;
 
     const queue = client.player.getQueue(int.guildId);
+    const track = queue.current;
+
+    const timestamp = queue.getPlayerTimestamp();
+    const trackDuration = timestamp.progress == 'Forever' ? 'Endless (Live)' : track.duration;
+    let description = `Author : **${track.author}**\nDuration **${trackDuration}**`;
 
     switch (int.customId) {
-        case 'saveTrack': {
-          if (!queue || !queue.playing) return int.reply({ content: `No music currently playing. ❌`, ephemeral: true, components: [] });
+        case 'Save Song': {
+            if (!queue || !queue.playing)
+                return int.reply({ content: `❌ | No music currently playing.`, ephemeral: true, components: [] });
 
-            int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
-                return int.reply({ content: `I sent you the name of the music in a private message ✅`, ephemeral: true, components: [] });
-            }).catch(error => {
-                return int.reply({ content: `I can't send you a private message. ❌`, ephemeral: true, components: [] });
-            });
+            int.member.send({ embeds: [embed.Embed_save(track.title, track.url, track.thumbnail, description)] })
+                .then(() => {
+                    return int.reply({ content: `✅ | I sent you the name of the music in a private message.`, ephemeral: true, components: [] });
+                }).catch(error => {
+                    return int.reply({ content: `❌ | I can't send you a private message.`, ephemeral: true, components: [] });
+                });
         }
     }
 };
