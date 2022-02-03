40d116d491993fee100b7714c315e6a8d200077d
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-02-03 21:42:08 +0800

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'play',
+  aliases: ['p'],
+  inVoiceChannel: true,
+  run: async (client, message, args) => {
+    const string = args.join(' ');
+    console.log(string)
+    if (!string)
+      return message.channel.send(`${client.emotes.error} | Please enter a song url or query to search.`);
+
+    client.distube.play(message.member.voice.channel, string, {
+      member: message.member,
+      textChannel: message.channel,
+      message
+    })
+  }
+}
