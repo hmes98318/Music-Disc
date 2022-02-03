3cc3d885ee9c860b115154e3e870ff9fc0a8e233
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-02-04 06:14:04 +0800

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,17 +1,41 @@
-module.exports = {
-  name: 'play',
-  aliases: ['p'],
-  inVoiceChannel: true,
-  run: async (client, message, args) => {
-    const string = args.join(' ');
-    console.log(string)
-    if (!string)
-      return message.channel.send(`${client.emotes.error} | Please enter a song url or query to search.`);
+const { QueryType } = require('discord-player');
+const embed = require('../embeds/embeds.js');
 
-    client.distube.play(message.member.voice.channel, string, {
-      member: message.member,
-      textChannel: message.channel,
-      message
-    })
-  }
-}
+module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    utilisation: '{prefix}play [song name/URL]',
+    voiceChannel: true,
+
+    async execute(client, message, args) {
+        if (!args[0])
+            return message.channel.send(`❌ | Write the name of the music you want to search.`);
+
+        const res = await client.player.search(args.join(' '), {
+            requestedBy: message.member,
+            searchEngine: QueryType.AUTO
+        });
+
+        if (!res || !res.tracks.length)
+            return message.channel.send(`❌ | No results found.`);
+
+        const queue = await client.player.createQueue(message.guild, {
+            metadata: message.channel
+        });
+
+        try {
+            if (!queue.connection)
+                await queue.connect(message.member.voice.channel);
+        } catch {
+            await client.player.deleteQueue(message.guild.id);
+            return message.channel.send(`❌ | I can't join audio channel.`);
+        }
+
+        await message.react('👍');
+
+        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+
+        if (!queue.playing)
+            await queue.play();
+    },
+};
