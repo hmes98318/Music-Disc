c4e50b8a553efe3059e003856158e212391d48e5
hmes98318 <s0925677069@gmail.com>
2022-09-29 22:52:10 +0800

    Update event 'ready'

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}`);
+    console.log(`>>> Logged in as ${client.user.username}`);
 
     client.user.setActivity(client.config.playing);
 };
