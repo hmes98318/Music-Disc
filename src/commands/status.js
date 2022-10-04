2764fb4378e6ef1a093c34d2d9285de82d0e5728
hmes98318 <s0925677069@gmail.com>
2022-10-05 00:14:35 +0800

    Fix fetch uptime error and rewrite 'status'

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,15 +1,22 @@
-const Discord = require('discord.js');
 const os = require('os');
-const exec = require('child_process').exec;
 const embed = require('../embeds/embeds');
 
-const uptime = new Date();
 
 module.exports = {
     name: 'status',
     aliases: ['usage'],
-    async execute(client, message) {//uptime, os, node_v, djs_v,cpu, cpu_usage, ram, ping
-        return message.channel.send({ embeds: [embed.Embed_status(Uptime(uptime), await OSversion(), process.version, `v${Discord.version}`, os.cpus()[0].model, Usage(), Math.round((os.totalmem() - os.freemem()) / (1000 * 1000)) + 'MB', client.ws.ping)] });
+    async execute(client, message) { //uptime, os, node_v, djs_v, cpu, cpu_usage, ram, ping
+        return message.channel.send({
+            embeds: [embed.Embed_status(
+                Uptime(client.status.uptime),
+                client.status.os_version,
+                client.status.node_version,
+                client.status.discord_version,
+                os.cpus()[0].model, Usage(),
+                Math.round((os.totalmem() - os.freemem()) / (1000 * 1000)) + 'MB',
+                client.ws.ping
+            )]
+        });
     }
 }
 
@@ -31,36 +38,15 @@ function Uptime(uptime) {
     let afterMin = Math.round(total - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);//取得算出分後剩餘的秒數
     console.log(day + ' / ' + hour + ':' + min + ':' + afterMin);
 
-    return /*day + ' Days' +*/ hour + 'Hour(s) ' + min + 'Minute(s)' /*+ afterMin*/;
+
+    if (day >= 1)
+        return day + ' Day(s) ' + hour + 'Hour(s)'/* + min + 'Minute(s)' + afterMin*/;
+    else
+        return /*day + ' Days' +*/ hour + 'Hour(s) ' + min + 'Minute(s)' /*+ afterMin + 'Second(s)'*/;
 }
 
 function Usage() {
     console.log(os.loadavg());
     let avg_load = os.loadavg();
     return avg_load[0] + '%';
-}
-
-function OSversion() {
-    let platform = process.platform;
-
-    if (platform === "win32")
-        return os.type();
-
-    else if (platform === "linux")
-        return new Promise(function (resolve, reject) {
-            exec('cat /etc/*release | grep -E ^PRETTY_NAME',
-                (error, stdout, stderr) => {
-                    let os_version = stdout.split('"')[1];
-                    //console.log(`${stdout}`);
-                    //console.log(`${stderr}`);
-
-                    if (error !== null) {
-                        reject(error);
-                    }
-                    resolve(os_version);
-                });
-        });
-
-    else
-        return process.platform;
 }
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,38 @@
+const Discord = require('discord.js');
+const os = require('os');
+
+
 module.exports = async (client) => {
     console.log(`>>> Logged in as ${client.user.username}`);
 
     client.user.setActivity(client.config.playing);
+
+    client.status = {
+        uptime: new Date(),
+        os_version: await OSversion(),
+        node_version: process.version,
+        discord_version: `v${Discord.version}`
+    };
 };
+
+
+function OSversion() {
+    let platform = process.platform;
+
+    if (platform === "win32")
+        return os.type();
+
+    else if (platform === "linux")
+        return new Promise(function (resolve, reject) {
+            exec('cat /etc/*release | grep -E ^PRETTY_NAME',
+                (error, stdout, stderr) => {
+                    if (error !== null) reject(error);
+
+                    let os_version = stdout.split('"')[1];
+                    resolve(os_version);
+                });
+        });
+
+    else
+        return process.platform;
+}
