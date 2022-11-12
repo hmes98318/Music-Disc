72b298ec6ec5291721384522db7c66ef41854cd6
contateste584394857 <contatesteqw5@gmail.com>
2022-11-12 15:17:42 -0400

    Select musics on play

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,5 +1,5 @@
 const { QueryType } = require('discord-player');
-
+const { SelectMenuBuilder, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
     name: 'play',
@@ -35,12 +35,34 @@ module.exports = {
             await client.player.deleteQueue(message.guild.id);
             return message.channel.send(`❌ | I can't join audio channel.`);
         }
+        if(res.playlist) {
+            queue.addTracks(res.tracks)
+            await message.react('👍');
+        } else {
+            let select = new SelectMenuBuilder()
+            .setCustomId("musicselect")
+            .setPlaceholder("Select the music")
+            .setOptions(res.tracks.map(x => {
+                return {
+                    label: x.title.length >= 25 ? x.title.substring(0, 22) + "..." : x.title,
+                    description: x.title.length >= 25 ? `${x.title} [${x.duration}]`.substring(0, 100) : `Duration: ${x.duration}`,
+                    value: x.id
+                }
+            }))
+            let row = new ActionRowBuilder().addComponents(select)
+            let msg = await message.reply({components: [row]})
 
-        await message.react('👍');
-
-        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
-
-        if (!queue.playing)
-            await queue.play();
+            let collector = msg.createMessageComponentCollector({time: 30000})
+            collector.on("collect", async i => {
+                if(i.customId != "musicselect" || i.user.id != message.author.id) return;
+                queue.addTrack(res.tracks.find(x => x.id == i.values[0]))
+                if (!queue.playing) await queue.play();
+                i.deferUpdate()
+                msg.edit({content: "✅ | Music added", components: []})
+            })
+            collector.on("end", (collected, reason) => {
+                if(reason == "time" && collected.size == 0) msg.edit({content: "❌ | Time expired.", components: []})
+            })
+        }
     },
 };
