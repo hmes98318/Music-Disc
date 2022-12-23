8d26f6858957d5b0d0420f17d11f3c24fb6e08a1
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-12-23 14:59:54 +0800

    Update `server`

--- src/commands/server.js
+++ src/commands/server.js
@@ -9,17 +9,19 @@ module.exports = {
 
     execute(client, message) {
         let serverlist = '';
-        client.guilds.cache.forEach((guild) => {
-            serverlist = serverlist.concat(" - **" + guild.name + "** ID: " + guild.id + "\n")
-        })
+        serverlist = client.guilds.cache
+            .map(g => `Guild ID: ${g.id}\n Guild: ${g.name}\n Members: ${g.memberCount}`)
+            .join('\n\n');
+
         return message.reply({ embeds: [embed.Embed_server(serverlist)], allowedMentions: { repliedUser: false } });
     },
 
     slashExecute(client, interaction) {
         let serverlist = '';
-        client.guilds.cache.forEach((guild) => {
-            serverlist = serverlist.concat(" - **" + guild.name + "** ID: " + guild.id + "\n")
-        })
+        serverlist = client.guilds.cache
+            .map(g => `Guild ID: ${g.id}\n Guild: ${g.name}\n Members: ${g.memberCount}`)
+            .join('\n\n');
+
         return interaction.reply({ embeds: [embed.Embed_server(serverlist)], allowedMentions: { repliedUser: false } });
     },
 };
