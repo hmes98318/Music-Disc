26bd44ee07b5c0163274b04245e075eccb23b7c4
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-03-03 20:40:20 +0800

    Fix voice server connection crash every minute

--- index.js
+++ index.js
@@ -215,3 +215,19 @@ player.on('channelEmpty', (queue) => {
     if (!client.config.autoLeave)
         queue.stop();
 });
+
+
+player.on('connectionCreate', (queue) => {
+    queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+        const oldNetworking = Reflect.get(oldState, 'networking');
+        const newNetworking = Reflect.get(newState, 'networking');
+
+        const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+            const newUdp = Reflect.get(newNetworkState, 'udp');
+            clearInterval(newUdp?.keepAliveInterval);
+        }
+
+        oldNetworking?.off('stateChange', networkStateChangeHandler);
+        newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
+});
