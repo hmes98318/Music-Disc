1ca61e15c6c4653803b5b3c923203cc17b0d2aa4
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-05-15 01:38:03 +0800

    Fix not showing typing before sending

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,29 +2,27 @@ const { color } = require(`${__dirname}/../utils/constants`);
 
 
 module.exports = (client, message) => {
-    if (message.author.bot || message.channel.type === 'dm')
-        return;
+    if (message.author.bot || message.channel.type === 'dm') return;
 
     const prefix = client.config.prefix;
+    if (message.content.indexOf(prefix) !== 0) return;
 
-    if (message.content.indexOf(prefix) !== 0)
-        return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
 
     if (cmd && cmd.voiceChannel) {
         if (!message.member.voice.channel)
-            return message.channel.send(`❌ | You are not connected to an audio channel.`);
+            return message.reply({ content: `❌ | You are not connected to an audio channel.`, allowedMentions: { repliedUser: false } });
 
         if (message.guild.members.me.voice.channel && message.member.voice.channelId !== message.guild.members.me.voice.channelId)
-            return message.channel.send(`❌ | You are not on the same audio channel as me.`);
+            return message.reply({ content: `❌ | You are not on the same audio channel as me.`, allowedMentions: { repliedUser: false } });
     }
 
     if (cmd) {
         console.log(`(${color.grey}${message.guild.name}${color.white}) ${message.author.username} : ${message.content}`);
+        message.channel.sendTyping();
         cmd.execute(client, message, args);
     }
 };
