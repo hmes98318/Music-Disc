a0c19c4a5ab642f99a2e46742fcf2b678710141c
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-07-04 22:36:46 +0800

    Add `pause`, `resume` command

---
+++ src/commands/pause.ts
@@ -0,0 +1,41 @@
+import { ChatInputCommandInteraction, Client, Message } from "discord.js";
+
+
+export const name = 'pause';
+export const aliases = [];
+export const description = 'Pause current track';
+export const usage = 'pause'
+export const voiceChannel = true;
+export const showHelp = true;
+export const options = [];
+
+
+export const execute = async (client: Client, message: Message) => {
+    const player = client.lavashark.getPlayer(message.guild!.id);
+
+    if (!player) {
+        return message.reply({ content: '❌ | There is no music currently playing.', allowedMentions: { repliedUser: false } });
+    }
+
+    if(player.paused) {
+        return message.reply({ content: '❌ | The music has been paused.', allowedMentions: { repliedUser: false } });
+    }
+
+    const SUCCESS = await player.pause();
+    return SUCCESS ? message.react('⏸️') : message.react('❌');
+}
+
+export const slashExecute = async (client: Client, interaction: ChatInputCommandInteraction) => {
+    const player = client.lavashark.getPlayer(interaction.guild!.id);
+
+    if (!player) {
+        return interaction.reply({ content: '❌ | There is no music currently playing.', allowedMentions: { repliedUser: false } });
+    }
+
+    if(player.paused) {
+        return interaction.reply({ content: '❌ | The music has been paused.', allowedMentions: { repliedUser: false } });
+    }
+
+    const SUCCESS = await player.pause();
+    return SUCCESS ? interaction.reply("⏸️ | Music paused.") : interaction.reply('❌ | Music pause failed.');
+}
---
+++ src/commands/resmue.ts
@@ -0,0 +1,41 @@
+import { ChatInputCommandInteraction, Client, Message } from "discord.js";
+
+
+export const name = 'resume';
+export const aliases = [];
+export const description = 'Resume paused track';
+export const usage = 'resume'
+export const voiceChannel = true;
+export const showHelp = true;
+export const options = [];
+
+
+export const execute = async (client: Client, message: Message) => {
+    const player = client.lavashark.getPlayer(message.guild!.id);
+
+    if (!player) {
+        return message.reply({ content: '❌ | There is no music currently playing.', allowedMentions: { repliedUser: false } });
+    }
+
+    if(!player.paused) {
+        return message.reply({ content: '❌ | The music has been resumed.', allowedMentions: { repliedUser: false } });
+    }
+
+    const SUCCESS = await player.resume();
+    return SUCCESS ? message.react('▶️') : message.react('❌');
+}
+
+export const slashExecute = async (client: Client, interaction: ChatInputCommandInteraction) => {
+    const player = client.lavashark.getPlayer(interaction.guild!.id);
+
+    if (!player) {
+        return interaction.reply({ content: '❌ | There is no music currently playing.', allowedMentions: { repliedUser: false } });
+    }
+
+    if(!player.paused) {
+        return interaction.reply({ content: '❌ | The music has been resumed.', allowedMentions: { repliedUser: false } });
+    }
+
+    const SUCCESS = await player.resume();
+    return SUCCESS ? interaction.reply("▶️ | Music resumed.") : interaction.reply('❌ | Music pause failed.');
+}
