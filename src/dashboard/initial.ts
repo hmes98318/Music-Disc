444b1e6f59272fe07cd0a247f30fd9112892ae8d
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-07-28 16:52:15 +0800

    Refactor dashboard initial method

--- src/dashboard/initial.ts
+++ src/dashboard/initial.ts
@@ -1,14 +1,6 @@
-import {
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    ChatInputCommandInteraction,
-    Client,
-    Message
-} from "discord.js";
+import { ChatInputCommandInteraction, Client, Message } from "discord.js";
 import { Player } from "lavashark";
 
-import { cst } from "../utils/constants";
 import { embeds } from "../embeds";
 
 
@@ -24,19 +16,12 @@ async function initial(client: Client, interactionOrMessage: ChatInputCommandInt
         channel = (interactionOrMessage as ChatInputCommandInteraction).channel;
     }
     else {
-        throw TypeError("Invalid Interaction or Message type");
+        throw new TypeError("Invalid Interaction or Message type");
     }
-
-    const playPauseButton = new ButtonBuilder().setCustomId('Dashboard-PlayPause').setEmoji(cst.button.pause).setStyle(ButtonStyle.Secondary);
-    const skipButton = new ButtonBuilder().setCustomId('Dashboard-Skip').setEmoji(cst.button.skip).setStyle(ButtonStyle.Secondary);
-    const stopButton = new ButtonBuilder().setCustomId('Dashboard-Stop').setEmoji(cst.button.stop).setStyle(ButtonStyle.Danger);
-    const loopButton = new ButtonBuilder().setCustomId('Dashboard-Loop').setEmoji(cst.button.loop).setStyle(ButtonStyle.Secondary);
-    const shuffleButton = new ButtonBuilder().setCustomId('Dashboard-Shuffle').setEmoji(cst.button.shuffle).setStyle(ButtonStyle.Secondary);
-    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(playPauseButton, skipButton, stopButton, loopButton, shuffleButton);
 
     player.dashboard = await channel!.send({
         embeds: [embeds.connected(client.config.embedsColor)],
-        components: [row]
+        components: []
     });
 }
 
