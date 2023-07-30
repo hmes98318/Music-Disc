4bb77262542956e39585b372fb186f622b5aeb90
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-07-30 13:47:31 +0800

    Update the node startup checker

--- src/index.ts
+++ src/index.ts
@@ -151,18 +151,25 @@ const loadCommands = () => {
 
 const checkNodesStats = async (nodes: Node[]) => {
     console.log(`-> Checking stats for all nodes ......`);
+
+    const pingList = await client.lavashark.nodesPing();
+
     console.log(`+--------------------------------+`);
-    for (const node of nodes) {
-        try {
-            await node.getStats();
-            console.log(`| ${node.identifier}: ${cst.color.green}CONNECTED${cst.color.white}`.padEnd(42, ' ') + '|');
-        } catch (_) {
+    for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+        const ping = pingList[i];
+
+        if (ping === -1) {
             console.log(`| ${node.identifier}: ${cst.color.red}DISCONNECTED${cst.color.white}`.padEnd(42, ' ') + '|');
+        }
+        else {
+            console.log(`| ${node.identifier}: ${cst.color.green}CONNECTED${cst.color.white}${cst.color.grey} (${ping}ms)${cst.color.white}`.padEnd(50, ' ') + '|');
         }
     }
     console.log(`+--------------------------------+`);
     console.log(`${cst.color.grey}-- All node stats have been checked --${cst.color.white}`);
 };
+
 
 Promise.resolve()
     .then(() => setEnvironment())
