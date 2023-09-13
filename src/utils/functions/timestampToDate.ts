f05d4ea182202c3f4d35c3e68c5abd5f140c3d45
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-09-13 23:11:11 +0800

    Add timestamp to date converter

---
+++ src/utils/functions/timestampToDate.ts
@@ -0,0 +1,21 @@
+const timestampToDate = (timestamp: number) => {
+    const date = new Date(timestamp);
+
+    // Get date
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    // Get time
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    // Format date & time
+    const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    return `${formattedDate} ${formattedTime}`;
+}
+
+export { timestampToDate };
