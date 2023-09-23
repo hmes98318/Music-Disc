b9e0314a9e7250f3a69fc3968c0f3b3916f20702
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-09-24 00:46:33 +0800

    Update `status` command

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -18,6 +18,7 @@ export const options = [];
 export const execute = async (client: Client, message: Message) => {
     const botPing = `${Date.now() - message.createdTimestamp}ms`;
     const sysload = await sysusage.cpu();
+    const pingList = await client.lavashark.nodesPing();
 
     const systemStatus = {
         load: sysload,
@@ -31,8 +32,32 @@ export const execute = async (client: Client, message: Message) => {
         serverCount: client.guilds.cache.size
     }
 
+    const nodes = client.lavashark.nodes;
+    const nodesStatus = [];
+    let healthValue = 0;
+
+    for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+        const ping = pingList[i];
+
+        if (ping === -1) {
+            healthValue++;
+            nodesStatus.push({ name: `❌ ${node.identifier}`, value: '**DISCONNECTED**' });
+        }
+        else {
+            nodesStatus.push({ name: `✅ ${node.identifier}`, value: `ping: **${ping}ms**` });
+        }
+    }
+    console.log('nodesStatus', nodesStatus);
+
+    const nodeHealth = healthValue === 0 ? 'All nodes are active' : `⚠️ There are ${healthValue} nodes disconnected`;
+
+
     return message.reply({
-        embeds: [embeds.botStatus(client.config, client.info, systemStatus)],
+        embeds: [
+            embeds.botStatus(client.config, client.info, systemStatus),
+            embeds.nodesStatus(client.config.embedsColor, nodeHealth, nodesStatus)
+        ],
         allowedMentions: { repliedUser: false }
     });
 }
@@ -40,6 +65,7 @@ export const execute = async (client: Client, message: Message) => {
 export const slashExecute = async (client: Client, interaction: ChatInputCommandInteraction) => {
     const botPing = `${Date.now() - interaction.createdTimestamp}ms`;
     const sysload = await sysusage.cpu();
+    const pingList = await client.lavashark.nodesPing();
 
     const systemStatus = {
         load: sysload,
@@ -53,8 +79,32 @@ export const slashExecute = async (client: Client, interaction: ChatInputCommand
         serverCount: client.guilds.cache.size
     }
 
+    const nodes = client.lavashark.nodes;
+    const nodesStatus = [];
+    let healthValue = 0;
+
+    for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+        const ping = pingList[i];
+
+        if (ping === -1) {
+            healthValue++;
+            nodesStatus.push({ name: `❌ ${node.identifier}`, value: '**DISCONNECTED**' });
+        }
+        else {
+            nodesStatus.push({ name: `✅ ${node.identifier}`, value: `ping: **${ping}ms**` });
+        }
+    }
+    console.log('nodesStatus', nodesStatus);
+
+    const nodeHealth = healthValue === 0 ? 'All nodes are active' : `⚠️ There are ${healthValue} nodes disconnected`;
+
+
     return interaction.editReply({
-        embeds: [embeds.botStatus(client.config, client.info, systemStatus)],
+        embeds: [
+            embeds.botStatus(client.config, client.info, systemStatus),
+            embeds.nodesStatus(client.config.embedsColor, nodeHealth, nodesStatus)
+        ],
         allowedMentions: { repliedUser: false }
     });
 }
