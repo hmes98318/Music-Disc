ca035f550046f1d9ad38572c581bbdf19738c4fe
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-11-03 17:58:01 +0800

    Speed ​​up socker.io nodes_status check

--- src/core/api/socketio.ts
+++ src/core/api/socketio.ts
@@ -3,12 +3,14 @@ import { uptime } from '../../utils/functions/uptime';
 
 import type { Server } from 'socket.io';
 import type { Client, VoiceChannel } from 'discord.js';
+import { NodeState } from 'lavashark';
 
 
 const registerSocketioEvents = (client: Client, io: Server) => {
 
     io.on('connection', (socket) => {
         // console.log('[socketio] a user connected');
+        //console.log('socket.cookie', socket.request.headers.cookie);
 
         /**
          * bot_status
@@ -40,39 +42,54 @@ const registerSocketioEvents = (client: Client, io: Server) => {
         socket.on("nodes_status", async () => {
             // console.log('[api] emit nodes_status');
 
-            let nodesStatus = [];
-            for (const node of client.lavashark.nodes) {
-                try {
-                    const nodeInfoPromise = node.getInfo();
-                    const nodeStatsPromise = node.getStats();
-                    const nodePingPromise = client.lavashark.nodePing(node);
-                    const timeoutPromise = new Promise((_, reject) => {
-                        setTimeout(() => {
-                            reject(new Error(`nodes_status "${node.identifier}" Timeout`));
-                        }, 1500);
-                    });
+            const nodePromises = client.lavashark.nodes.map(async (node) => {
+                if (node.state === NodeState.CONNECTED) {
+                    try {
+                        const nodeInfoPromise = node.getInfo();
+                        const nodeStatsPromise = node.getStats();
+                        const nodePingPromise = client.lavashark.nodePing(node);
+                        const timeoutPromise = new Promise((_, reject) => {
+                            setTimeout(() => {
+                                reject(new Error(`nodes_status "${node.identifier}" Timeout`));
+                            }, 1500);
+                        });
 
-                    const [nodeInfo, nodeStats, nodePing] = await (Promise.race([Promise.all([nodeInfoPromise, nodeStatsPromise, nodePingPromise]), timeoutPromise]) as Promise<[(typeof nodeInfoPromise), (typeof nodeStatsPromise), (typeof nodePingPromise)]>);
+                        const [nodeInfo, nodeStats, nodePing] = await (Promise.race([Promise.all([nodeInfoPromise, nodeStatsPromise, nodePingPromise]), timeoutPromise]) as Promise<[(typeof nodeInfoPromise), (typeof nodeStatsPromise), (typeof nodePingPromise)]>);
 
-                    nodesStatus.push({
-                        id: node.identifier,
-                        state: node.state,
-                        info: nodeInfo,
-                        stats: nodeStats,
-                        ping: nodePing
-                    });
-                } catch (_) {
-                    nodesStatus.push({
+                        return {
+                            id: node.identifier,
+                            state: node.state,
+                            info: nodeInfo,
+                            stats: nodeStats,
+                            ping: nodePing
+                        };
+                    } catch (_) {
+                        return {
+                            id: node.identifier,
+                            state: node.state,
+                            info: {},
+                            stats: {},
+                            ping: -1
+                        };
+                    }
+                }
+                else {
+                    return {
                         id: node.identifier,
                         state: node.state,
                         info: {},
                         stats: {},
-                        ping: -1
-                    });
+                        ping: -1,
+                    };
                 }
-            }
+            });
 
-            socket.emit('api_nodes_status', nodesStatus);
+            try {
+                const nodesStatus = await Promise.all(nodePromises);
+                socket.emit('api_nodes_status', nodesStatus);
+            } catch (error) {
+                console.error('Error while fetching node data:', error);
+            }
         });
 
         /**
