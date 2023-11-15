6d0596a8179170ab0bf10c9c0c6ab4723d75292c
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-11-16 01:55:46 +0800

    refactor: Initial loading structure for the Bot

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,17 @@ import {
     Message
 } from 'discord.js';
 import { LavaShark } from 'lavashark';
-import consoleStamp from 'console-stamp';
 
-import {
-    checkNodesStats,
-    loadSite,
-    loadBlacklist,
-    loadCommands,
-    loadDiscordEvents,
-    loadLavaSharkEvents,
-    loadLocalNode,
-    setEnvironment
-} from './core/loader';
-import { cst } from './utils/constants';
+import { App } from './core/App';
 import nodeList from '../nodelist.json';
 
-import type { Config, QueuePage, SystemInfo } from './@types';
+import type { QueuePage } from './@types';
 
 
 declare module 'discord.js' {
     export interface Client {
         commands: Collection<unknown, any>,
-        lavashark: LavaShark,
-        config: Config,
-        info: SystemInfo
+        lavashark: LavaShark
     }
 };
 
@@ -42,7 +29,6 @@ declare module 'lavashark' {
 };
 
 
-consoleStamp(console, { format: ':date(yyyy/mm/dd HH:MM:ss)' });
 
 
 let client = new Client({
@@ -58,26 +44,15 @@ client.commands = new Collection();
 client.lavashark = new LavaShark({
     nodes: nodeList,
     sendWS: (guildId, payload) => { client.guilds.cache.get(guildId)?.shard.send(payload); }
-})
-client.config = cst.config;
+});
 
 
+const main = async () => {
+    const app = new App(client);
+    app.initialize();
+}
 
-
-Promise.resolve()
-    .then(() => setEnvironment(client))
-    .then(async () => { if (client.config.enableLocalNode) await loadLocalNode(client) })
-    .then(() => loadDiscordEvents(client))
-    .then(() => loadLavaSharkEvents(client))
-    .then(() => loadCommands(client))
-    .then(() => loadBlacklist(client))
-    .then(async () => { if (client.config.enableSite) await loadSite(client) })
-    .then(() => checkNodesStats(client))
-    .then(() => {
-        console.log(`${cst.color.green}*** All loaded successfully ***${cst.color.white}`);
-        client.login(process.env.BOT_TOKEN);
-    });
-
+main();
 
 
 
