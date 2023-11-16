47d46d54e75ec964b5f2f7456f57aeff5a3a8a07
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-11-17 02:43:30 +0800

    feat: Add localNode constants variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,7 +15,6 @@ export const cst = {
         autoLeave           : true,
         autoLeaveCooldown   : 5000,
         displayVoiceState   : true,
-        blacklist           : [],           // It must be the user ID (string[])
         enableSite          : true,
         site: {
             port            : 33333,
@@ -23,7 +22,11 @@ export const cst = {
             password        : 'password'
         },
         enableLocalNode     : false,
+        localNode:{
+            autoRestart     : true
+        }
     },
+    blacklist               : [],           // It must be the user ID (string[])
     // Console color
     color: {
         cyan    : '\x1B[36m',
@@ -44,7 +47,7 @@ export const cst = {
         shuffle     : '<:w_shuffle:1106270712542531624>',
         prev        : '<:w_prev:1153665768093921280>',
         next        : '<:w_next:1153665809990815874>',
-        delete      : 'Delete',
+        delete      : 'Delete Message',
         clear       : 'Clear Queue'
     }
 };
