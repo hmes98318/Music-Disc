9bfab1941ebccdc9e237e1fc1e1fb83cf66bec99
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2025-09-01 23:31:24 +0800

    chore: Remove debug messages

--- src/events/lavashark/debug.ts
+++
@@ -1,7 +0,0 @@
-import type { Client } from 'discord.js';
-import type { Bot } from '../../@types/index.js';
-
-    
-export default async (bot: Bot, _client: Client, message: string) => {
-    bot.logger.emit('lavashark', bot.shardId, `[debug] ${message}`);
-};
