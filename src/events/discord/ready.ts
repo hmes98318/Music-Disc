dcac89f6d7c4a4aca191ef0cc3feff9505965ae5
hoehoetvhs <hoehoetvhs@daum.net>
2025-09-21 15:12:35 +0900

    Update ready.ts

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -1,13 +1,87 @@
+import { ChannelType, Client, ClientPresenceStatus } from 'discord.js';
+import { getSysInfo } from '../../utils/functions/getSysInfo.js';
 import { cst } from '../../utils/constants.js';
-import { ChannelType, ClientPresenceStatus } from 'discord.js';
-import type { Client } from 'discord.js';
+
 import type { Bot } from '../../@types/index.js';
 
-export default async (bot: Bot, client: Client) => {
-    // Log login information
-    bot.logger.emit('discord', bot.shardId, `>>> Logged in as ${client.user?.username}`);
 
-    // Set client status & activity
+export default async (bot: Bot, client: Client) => {
+    // Get system info and prepare for logging
+    bot.sysInfo = await getSysInfo();
+
+    const release = {
+        bot: `${bot.config.bot.name}: ${cst.color.cyan}${bot.sysInfo.bot_version}${cst.color.white}`,
+        nodejs: `Node.js:    ${cst.color.cyan}${bot.sysInfo.node_version}${cst.color.white}`,
+        djs: `Discord.js: ${cst.color.cyan}${bot.sysInfo.dc_version}${cst.color.white}`,
+        shark: `LavaShark:  ${cst.color.cyan}${bot.sysInfo.shark_version}${cst.color.white}`,
+    };
+
+    // Log version information
+    bot.logger.emit('log', bot.shardId, `+-----------------------+`);
+    bot.logger.emit('log', bot.shardId, `| ${release.bot.padEnd(30, ' ')} |`);
+    bot.logger.emit('log', bot.shardId, `| ${release.nodejs.padEnd(30, ' ')} |`);
+    bot.logger.emit('log', bot.shardId, `| ${release.djs.padEnd(30, ' ')} |`);
+    bot.logger.emit('log', bot.shardId, `| ${release.shark.padEnd(30, ' ')} |`);
+    bot.logger.emit('log', bot.shardId, `+-----------------------+`);
+
+
+    // Register slash commands if enabled
+    if (bot.config.bot.slashCommand) {
+        bot.logger.emit('log', bot.shardId, 'Enable slash command.');
+        client.application?.commands.set(client.commands.map(cmd => {
+            return {
+                name: cmd.name,
+                description: cmd.description,
+                options: cmd.options
+            };
+        }));
+    }
+    else {
+        bot.logger.emit('log', bot.shardId, 'Disable slash command.');
+    }
+
+
+    // Auto join voice channel after Lavashark node connection
+    client.lavashark.once('nodeConnect', async () => {
+        if ((bot.shardId + 1 >= (client.shard?.count ?? 1)) && bot.config.bot.startupAutoJoin) {
+            const channel = await client.channels.fetch(bot.config.bot.specifyVoiceChannel ?? '0');
+
+            if (!channel || channel.type !== ChannelType.GuildVoice) {
+                bot.logger.emit('log', bot.shardId, `The specified voice channel not found, set to disabled. (${bot.config.bot.specifyVoiceChannel})`);
+                bot.logger.emit('log', bot.shardId, `The specifyVoiceChannel value is incorrect, set startupAutoJoin to disabled.`);
+
+                bot.config.bot.specifyVoiceChannel = null;
+                bot.config.bot.startupAutoJoin = false;
+                return;
+            }
+
+            // Creates the audio player
+            const player = client.lavashark.createPlayer({
+                guildId: channel.guildId,
+                voiceChannelId: channel.id,
+                textChannelId: channel.id,
+                selfDeaf: true
+            });
+
+            if (!player.setting) {
+                player.setting = {
+                    queuePage: null,
+                    volume: null
+                };
+            }
+
+            try {
+                // Connects to the voice channel
+                await player.connect();
+                bot.logger.emit('log', bot.shardId, `Auto join voice channel : ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyVoiceChannel})`);
+            } catch (error) {
+                bot.logger.emit('error', bot.shardId, 'Error startup auto joining channel: ' + error);
+            }
+        }
+    });
+
+    // Start Lavashark and set bot presence
+    client.lavashark.start(String(client.user?.id));
     client.user?.setStatus(bot.config.bot.status as ClientPresenceStatus);
     client.user?.setActivity({
         name: bot.config.bot.activity.name,
@@ -26,84 +100,41 @@ export default async (bot: Bot, client: Client) => {
         });
     }, 10 * 60 * 1000); // every 10 minutes
 
-    // Auto join voice channel after startup
-    client.lavashark.once('nodeConnect', async () => {
-        if ((bot.shardId + 1 >= (client.shard?.count ?? 1)) && bot.config.bot.startupAutoJoin) {
-            const channel = await client.channels.fetch(bot.config.bot.specifyVoiceChannel ?? '0');
-
-            if (!channel || channel.type !== ChannelType.GuildVoice) {
-                bot.logger.emit('log', bot.shardId, `The specified voice channel not found, set to disabled. (${bot.config.bot.specifyVoiceChannel})`);
-                bot.logger.emit('log', bot.shardId, `The specifyVoiceChannel value is incorrect, set startupAutoJoin to disabled.`);
-
-                bot.config.bot.specifyVoiceChannel = null;
-                bot.config.bot.startupAutoJoin = false;
-
-                return;
-            }
-
-            // Create the audio player
-            const player = client.lavashark.createPlayer({
-                guildId: channel.guildId,
-                voiceChannelId: channel.id,
-                textChannelId: channel.id,
-                selfDeaf: true
-            });
-
-            if (!player.setting) {
-                player.setting = {
-                    queuePage: null,
-                    volume: null
-                };
-            }
-
-            try {
-                // Connect to the voice channel
-                await player.connect();
-                bot.logger.emit('log', bot.shardId, `Auto joined voice channel: ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyVoiceChannel})`);
-            } catch (error) {
-                bot.logger.emit('error', bot.shardId, 'Error during auto join: ' + error);
-            }
-        }
-    });
-
-    client.lavashark.start(String(client.user?.id));
 
     // Check specified message channel ID
     if (bot.config.bot.specifyMessageChannel) {
         const channel = await client.channels.fetch(bot.config.bot.specifyMessageChannel);
-
         if (!channel) {
             bot.logger.emit('log', bot.shardId, `The specified message channel not found, set to disabled. (${bot.config.bot.specifyMessageChannel})`);
             bot.config.bot.specifyMessageChannel = null;
-        } else {
-            bot.logger.emit('log', bot.shardId, `Set specified message channel: ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyMessageChannel})`);
+        }
+        else {
+            bot.logger.emit('log', bot.shardId, `Set specify message channel : ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyMessageChannel})`);
         }
     }
 
     // Check specified voice channel ID
     if (bot.config.bot.specifyVoiceChannel) {
         const channel = await client.channels.fetch(bot.config.bot.specifyVoiceChannel);
-
         if (!channel) {
             bot.logger.emit('log', bot.shardId, `The specified voice channel not found, set to disabled. (${bot.config.bot.specifyVoiceChannel})`);
             bot.logger.emit('log', bot.shardId, `The specifyVoiceChannel value is incorrect, set startupAutoJoin to disabled.`);
-
             bot.config.bot.specifyVoiceChannel = null;
             bot.config.bot.startupAutoJoin = false;
-        } else {
-            bot.logger.emit('log', bot.shardId, `Set specified voice channel: ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyVoiceChannel})`);
-
+        }
+        else {
+            bot.logger.emit('log', bot.shardId, `Set specify voice channel : ${(channel as any).name || 'Unknown channel'} (${bot.config.bot.specifyVoiceChannel})`);
             if (bot.config.bot.startupAutoJoin) {
-                bot.logger.emit('log', bot.shardId, `StartupAutoJoin Enabled.`);
-            } else {
-                bot.logger.emit('log', bot.shardId, `StartupAutoJoin Disabled.`);
+                bot.logger.emit('log', bot.shardId, `Set startupAutoJoin Enabled.`);
+            }
+            else {
+                bot.logger.emit('log', bot.shardId, `Set startupAutoJoin to disabled.`);
             }
         }
     }
 
-    // Set admin user IDs
-    bot.logger.emit('log', bot.shardId, `Admin user IDs: ${JSON.stringify(bot.config.bot.admin)}`);
-
-    // Shard launch complete
+    // Log final setup information
+    bot.logger.emit('log', bot.shardId, `Set admin as user ID : ${JSON.stringify(bot.config.bot.admin)}`);
+    bot.logger.emit('discord', bot.shardId, `>>> Logged in as ${client.user?.username}`);
     bot.logger.emit('log', bot.shardId, `${cst.color.green}*** Launched shard ${bot.shardId + 1} / ${client.shard?.count} ***${cst.color.white}`);
 };
