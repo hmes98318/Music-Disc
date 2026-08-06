141d94c261ea3a0fcce943846bb607bcae995e19
hoehoetvhs <hoehoetvhs@daum.net>
2026-08-06 10:12:26 +0900

    feat: set volume-default command as admin-only permission

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -156,8 +156,10 @@ export const cst = {
         // Command permission settings
         command: {
             disableCommand      : [],                                   // Disabled commands, all enabled by default
-            adminCommand        : ['language','server', 'status'],      // Admin commands, only Admin role user can use
+            adminCommand        : ['language', 'server', 'status', 'volume-default'], // Admin commands, only Admin role user can use
             djCommand           : ['dj'],                               // DJ commands, only DJ role user can use
+
+
             requesterOnly       : ['skip'],                              // Commands restricted to the song requester
             requesterDjBypass   : ['skip']                              // Commands DJs can bypass requesterOnly on
         },
